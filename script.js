a0ff24afccecbeeb607ebcee7b13db6c7bc1eee8
E132 <107452563+132E@users.noreply.github.com>
2022-07-07 13:41:58 -0500

    worked on the API call

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const button = document.querySelector("button");
 const mealList = document.querySelector("input");
+const mealHeaderText = document.querySelector("#meal-header");
+const mealAreaText = document.querySelector("#meal-area");
+const mealDescText = document.querySelector("#meal-desc");
+
 const imageDiv = document.getElementById("img-div");
-
-const API_ID = "1";
-const API_key = "1";
-
-button.addEventListener("click", async () => {
+console.log("working");
+button.addEventListener("click", async (e) => {
+  e.preventDefault();
   let meal = mealList.value;
   let response = await axios.get(
-    `https://www.themealdb.com/api/json/v1/1/search.php?s=chicken&api_id=${API_ID}&api_key=${API_key}`
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`
   );
-  let mealPic = response.data.message;
+  let meals = response.data.meals;
+  console.log(meals[0]);
+  let mealPic = meals[0].strMealThumb;
+  let mealHeader = meals[0].strMeal;
+  let mealDesc = meals[0].strInstructions;
+  let mealArea = meals[0].strArea;
+
+  mealHeaderText.innerHTML = mealHeader;
+  mealAreaText.innerHTML = mealArea;
+  mealDescText.innerHTML = mealDesc;
   imageDiv.innerHTML = `<img src=${mealPic}>`;
 });
